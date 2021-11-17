fea4373548cacfe8cd06776bbe53f5d8dee34a08
tsntudy <94297053+tsntudy@users.noreply.github.com>
2021-11-18 00:25:14 +0900

    Update simpleCalcTest.java

--- src/test/java/simpleCalc/simpleCalcTest.java
+++ src/test/java/simpleCalc/simpleCalcTest.java
@@ -47,7 +47,7 @@ public class simpleCalcTest {
     	
     	/*期待値を入力*/
     	String actual = "計算式を入力して下さい:入力された式は 5.0 * 2.0 です\r\n" +
-    			"計算結果は 10.0 です";
+    			"計算結果は 10.0 です\r\n";
     	
     	/*期待値と実行した標準出力の値が等しいか確認*/
     	assertEquals(actual,outContent.toString());
@@ -59,7 +59,7 @@ public class simpleCalcTest {
     	in.inputln("10 - 11");
     	simpleCalc.main(args);
     	String actual = "計算式を入力して下さい:入力された式は 10.0 - 11.0 です\r\n" +
-    			"計算結果は -1.0 です ";
+    			"計算結果は -1.0 です \r\n";
     	assertEquals(actual,outContent.toString());
     }
 
@@ -69,7 +69,7 @@ public class simpleCalcTest {
     	in.inputln("99999 + 1");
     	simpleCalc.main(args);
     	String actual = "計算式を入力して下さい:入力された式は 99999.0 + 1.0 です\r\n" +
-    			"計算結果は 100000.0 です \r\n";
+    			"計算結果は 100000.0 です\r\n";
     	assertEquals(actual,outContent.toString());
     }
 
@@ -79,7 +79,7 @@ public class simpleCalcTest {
     	in.inputln("1 / 3");
     	simpleCalc.main(args);
     	String actual = "計算式を入力して下さい:入力された式は 1.0 / 3.0 です\r\n" +
-    			"計算結果は 0.3333333333333333 です" + " ";
+    			"計算結果は 0.3333333333333333 です" + " \r\n";
     	assertEquals(actual,outContent.toString());
     }
 
@@ -89,7 +89,7 @@ public class simpleCalcTest {
     	in.inputln("5 % 2");
     	simpleCalc.main(args);
     	String actual = "計算式を入力して下さい:入力された式は 5.0 % 2.0 です\r\n" +
-    			"計算結果は 1.0 です \r\n" + " ";
+    			"計算結果は 1.0 です　\r\n";
     	assertEquals(actual,outContent.toString());
     }
 
