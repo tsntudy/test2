2d385db1ee634c5e5e40f47117b11d226d674ab1
tsntudy <94297053+tsntudy@users.noreply.github.com>
2021-11-18 01:34:45 +0900

    delete \r

--- src/test/java/simpleCalc/simpleCalcTest.java
+++ src/test/java/simpleCalc/simpleCalcTest.java
@@ -47,8 +47,8 @@ public class simpleCalcTest {
     	simpleCalc.main(args); /*mainメソッドを実行*/
 
     	/*期待値を入力*/
-    	String actual = "計算式を入力して下さい:入力された式は 5.0 * 2.0 です\r\n" +
-    			"計算結果は 10.0 です\r\n";
+    	String actual = "計算式を入力して下さい:入力された式は 5.0 * 2.0 です\n" +
+    			"計算結果は 10.0 です\n";
 
     	/*期待値と実行した標準出力の値が等しいか確認*/
     	assertThat(actual, is(outContent.toString()));
@@ -59,8 +59,8 @@ public class simpleCalcTest {
     	String[] args = {};
     	in.inputln("10 - 11");
     	simpleCalc.main(args);
-    	String actual = "計算式を入力して下さい:入力された式は 10.0 - 11.0 です\r\n" +
-    			"計算結果は -1.0 です\r\n";
+    	String actual = "計算式を入力して下さい:入力された式は 10.0 - 11.0 です\n" +
+    			"計算結果は -1.0 です\n";
     	assertThat(actual, is(outContent.toString()));
     }
 
@@ -69,8 +69,8 @@ public class simpleCalcTest {
     	String[] args = {};
     	in.inputln("99999 + 1");
     	simpleCalc.main(args);
-    	String actual = "計算式を入力して下さい:入力された式は 99999.0 + 1.0 です\r\n" +
-    			"計算結果は 100000.0 です\r\n";
+    	String actual = "計算式を入力して下さい:入力された式は 99999.0 + 1.0 です\n" +
+    			"計算結果は 100000.0 です\n";
     	assertThat(actual, is(outContent.toString()));
     }
 
@@ -79,8 +79,8 @@ public class simpleCalcTest {
     	String[] args = {};
     	in.inputln("1 / 3");
     	simpleCalc.main(args);
-    	String actual = "計算式を入力して下さい:入力された式は 1.0 / 3.0 です\r\n" +
-    			"計算結果は 0.3333333333333333 です\r\n";
+    	String actual = "計算式を入力して下さい:入力された式は 1.0 / 3.0 です\n" +
+    			"計算結果は 0.3333333333333333 です\n";
     	assertThat(actual, is(outContent.toString()));
     }
 
